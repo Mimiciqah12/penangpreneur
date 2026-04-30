17642071425a9d1ca1a6a4f6d363283dda32f64b
NUR FAQIHAH RASYIQAH <d20221101802@siswa.upsi.edu.my>
2026-04-30 12:29:09 +0800

    update model

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registration extends Model
 {
+    // Saya telah tambahkan address, premises_address, dan premises_image
     protected $fillable = [
         'full_name',
         'phone_number',
@@ -13,7 +14,10 @@ class Registration extends Model
         'agency_name',
         'business_category',
         'product_name',
+        'address',             
+        'premises_address',    
         'product_image',
+        'premises_image',      
         'product_description',
         'reference_number',
     ];
