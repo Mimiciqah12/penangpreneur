e433711b9e0bb3c63c64ce7c981dff7849104b81
NUR FAQIHAH RASYIQAH <d20221101802@siswa.upsi.edu.my>
2026-04-30 13:47:12 +0800

    new update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // Baris ini ditambah
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Blok kod ini ditambah untuk memaksa HTTPS di Render
+        // Memaksa sistem menggunakan pautan HTTPS (selamat) 
+        // hanya apabila ia tidak berada di localhost komputer ('local')
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
